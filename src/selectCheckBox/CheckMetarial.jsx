7779c590eef709a1e74fab8e17e115687371be55
seoyuun <carpedm1221@gmail.com>
2022-02-07 22:02:54 +0900

    Add: data 초기 셋팅, selectbox 컴포넌트 카테고리별 분리

---
+++ src/selectCheckBox/CheckMetarial.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export const selectBox2 = [
+  {
+    id: 0,
+    title: '알루미늄',
+  },
+  {
+    id: 1,
+    title: '탄소강',
+  },
+  {
+    id: 2,
+    title: '구리',
+  },
+  {
+    id: 3,
+    title: '합금강',
+  },
+  {
+    id: 4,
+    title: '강철',
+  },
+];
+
+const CheckMetarial = () => {
+  return (
+    <>
+      <select></select>
+    </>
+  );
+};
+
+export default CheckMetarial;
